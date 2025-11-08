bcda8cc2d3734e9459ab61d5108bf28fa577dcff
krekz <kfitri559@gmail.com>
2025-11-08 11:55:38 +0800

    feat: updated shadcn UI components with new variants and styling updates

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -82,7 +82,7 @@ function SidebarProvider({
 			}
 
 			// This sets the cookie to keep the sidebar state.
-			// biome-ignore lint/suspicious/noDocumentCookie: false positive
+			// biome-ignore lint/suspicious/noDocumentCookie: disable
 			document.cookie = `${SIDEBAR_COOKIE_NAME}=${openState}; path=/; max-age=${SIDEBAR_COOKIE_MAX_AGE}`;
 		},
 		[setOpenProp, open],
